c851bf2a5d6aee79cd60bbe1c1c0a1c48cff34d0
bisratberhanu <106919633+bisratberhanu@users.noreply.github.com>
2022-10-18 17:57:08 +0300

    Add files via upload

---
+++ max consecutive ones 3.py
@@ -0,0 +1,15 @@
+class Solution:
+    from typing import List
+    def longestOnes(self, nums: List[int], k: int) -> int:
+        left = 0
+        cntZeros = 0
+        ans = 0
+        for right in range(len(nums)):
+            if nums[right] == 0:
+                cntZeros += 1
+            while cntZeros > k:  
+                if nums[left] == 0: cntZeros -= 1
+                left += 1
+            ans = max(ans, right - left + 1)
+        return ans
+        
