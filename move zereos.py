8a7fea7ad9b6d6d478eef4044580a782c78dbb65
bisratberhanu <106919633+bisratberhanu@users.noreply.github.com>
2022-10-18 11:05:59 +0300

    Add files via upload

---
+++ move zereos.py
@@ -0,0 +1,11 @@
+def moveZeroes(self, nums):
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        pos = 0
+        
+        for i in range(len(nums)):
+            el = nums[i]
+            if el != 0:
+                nums[pos], nums[i] = nums[i], nums[pos]
+                pos += 1
