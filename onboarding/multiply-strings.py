48d087e3fc62ab0bd8babf1d3f67735813db17e8
bisratberhanu <106919633+bisratberhanu@users.noreply.github.com>
2023-12-12 17:54:09 +0300

    Added solution for Multiply Strings

---
+++ onboarding/multiply-strings.py
@@ -0,0 +1,12 @@
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        '''subtracted= ord('0')
+        lis1=[]
+        lis2=[]
+        for letter in num1:
+            lis1.append( ord(letter)-subtracted)
+        for letter in num2:
+            lis2.append(ord(letter)-subtracted)
+        for val in lis'''
+        return str(eval(num1+'*'+num2))
+        
