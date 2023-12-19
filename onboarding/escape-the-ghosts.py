751775b42d9afa2158c3d43c1082a777d022c432
bisratberhanu <106919633+bisratberhanu@users.noreply.github.com>
2023-12-19 11:17:08 +0300

    Added solution for Escape The Ghosts

---
+++ onboarding/escape-the-ghosts.py
@@ -0,0 +1,10 @@
+class Solution:
+    def escapeGhosts(self, ghosts: List[List[int]], target: List[int]) -> bool:
+        pac_dist= abs(target[0])+ abs(target[1])
+        for ghost in ghosts:
+            ghost_dist= abs(ghost[0]- target[0]) + abs(ghost[1]- target[1])
+            if ghost_dist<= pac_dist:
+                return False
+        return True
+
+         
