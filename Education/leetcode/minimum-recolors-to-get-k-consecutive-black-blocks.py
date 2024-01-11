354a374effad96f8ef8c7a5e072a33d710995e72
bisratberhanu <106919633+bisratberhanu@users.noreply.github.com>
2024-01-11 17:22:49 +0300

    Add solution for Minimum Recolors to Get K Consecutive Black Blocks

--- Education/leetcode/minimum-recolors-to-get-k-consecutive-black-blocks.py
+++ Education/leetcode/minimum-recolors-to-get-k-consecutive-black-blocks.py
@@ -1,15 +1,23 @@
 class Solution:
     def minimumRecolors(self, blocks: str, k: int) -> int:
-        counter_list=[]
+        white_blocks=0
         left=0
-        right=k-1
-        while right<len(blocks):
-            new_string=blocks[left:right+1]
-            counter=0
-            for letter in new_string:
-                if letter=='W':
-                    counter+=1
-            counter_list.append(counter)
-            right+=1
-            left+=1
-        return min(counter_list)
+        ans= float('inf')
+        for right in range(len(blocks)):
+            if right< k:
+                if blocks[right]=='W':
+                    white_blocks+=1
+                if right==k-1:
+                    
+                    ans= min(ans,white_blocks) 
+            else:
+                if blocks[left]=='W':
+                    white_blocks-=1
+                if blocks[right]=='W':
+                    white_blocks+=1
+                ans= min(ans,white_blocks)
+                left+=1
+            #print(ans)
+            #print(white_blocks)
+        ans= min(ans,white_blocks)
+        return ans 
