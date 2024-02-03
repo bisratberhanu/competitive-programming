c32fcd62922a0768d3505201daedbea127356b3f
bisratberhanu <106919633+bisratberhanu@users.noreply.github.com>
2024-02-03 22:06:05 +0300

    Add solution for Corporate Flight Bookings

---
+++ leetcode/corporate-flight-bookings.py
@@ -0,0 +1,13 @@
+class Solution:
+    def corpFlightBookings(self, bookings: List[List[int]], n: int) -> List[int]:
+        prefixsum=[0]*(n+2)
+        for booking in bookings:
+            start,end=booking[0],booking[1]
+            prefixsum[start]+= booking[2]
+            
+            prefixsum[end+1]-= booking[2]
+        #now calculating the prefic sum
+        for i in range(1,len(prefixsum)):
+            prefixsum[i]=prefixsum[i-1]+prefixsum[i]
+        return prefixsum[1:n+1]
+
