b2e5caf018d62ffde2f207b64651c22afc0892ae
Bisrat Berhanu Negus <106919633+bisratberhanu@users.noreply.github.com>
2024-04-26 19:01:01 +0300

    Created 'Single-Threaded CPU' by A2SV Hub

---
+++ 26-Apr-2024/Single-Threaded CPU 134443.py
@@ -0,0 +1,26 @@
+# Problem: Single-Threaded CPU - https://leetcode.com/problems/single-threaded-cpu/
+
+class Solution:
+    def getOrder(self, tasks: List[List[int]]) -> List[int]:
+        for i, t in enumerate(tasks):
+            t.append(i)
+        tasks.sort()
+        res, minheap=[],[]
+        i,time= 0, tasks[0][0]
+
+        while minheap or i< len(tasks):
+            while i< len(tasks) and time >= tasks[i][0]:
+                heapq.heappush(minheap, [tasks[i][1], tasks[i][2]])
+                i+=1
+            if not minheap:
+                time= tasks[i][0] 
+         
+                
+
+
+            else:
+                prostime, index= heapq.heappop(minheap)
+                time+= prostime
+                res.append(index)
+        return res
+
