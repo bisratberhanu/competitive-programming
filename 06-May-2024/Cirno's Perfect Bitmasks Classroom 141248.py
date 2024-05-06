b4ca20890841fae35ee8d9cb306f697050f0de2f
Bisrat Berhanu Negus <106919633+bisratberhanu@users.noreply.github.com>
2024-05-06 21:46:15 +0300

    Created 'Cirno's Perfect Bitmasks Classroom' by A2SV Hub

---
+++ 06-May-2024/Cirno's Perfect Bitmasks Classroom 141248.py
@@ -0,0 +1,26 @@
+# Problem: Cirno's Perfect Bitmasks Classroom - https://codeforces.com/problemset/problem/1688/A
+
+from math import log2
+t= int(input())
+
+def helper(num):
+    if num==1 or num==2:
+        print(3)
+        return
+    if log2(num)== int(log2(num)):
+        print(num+1)
+        return 
+    bit= bin(num)[2:]
+    counter=0
+    for i in range(len(bit)-1,-1,-1):
+        if bit[i]== '1':
+            candi= '1' + '0'*(counter)
+            if candi!= bit:
+                print(int(candi,2))
+                return
+        counter+=1
+    candi= '1' * (len(bit))
+    print(int(candi,2))
+for _ in range(t):
+    n= int(input())
+    helper(n)
