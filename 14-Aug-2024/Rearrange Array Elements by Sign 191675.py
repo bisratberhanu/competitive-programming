735c62e477d494f42287175c919a323b3f9262e4
Bisrat Berhanu Negus <106919633+bisratberhanu@users.noreply.github.com>
2024-08-14 23:50:18 +0300

    Created 'Rearrange Array Elements by Sign' by A2SV Hub

---
+++ 14-Aug-2024/Rearrange Array Elements by Sign 191675.py
@@ -0,0 +1,15 @@
+# Problem: Rearrange Array Elements by Sign - https://leetcode.com/problems/rearrange-array-elements-by-sign/description/
+
+class Solution:
+    def rearrangeArray(self, nums: List[int]) -> List[int]:
+        ans=[0 for i in range(len(nums))]
+        ptr1,ptr2=0,1
+        for i in range(len(nums)):
+            if nums[i]>0:
+                ans[ptr1]= nums[i]
+                ptr1+=2
+            else:
+                ans[ptr2]= nums[i]
+                ptr2+=2
+        return ans 
+        
