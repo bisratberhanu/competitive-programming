a88ace1e7c76e3d64d2609d1ff5e92ba4ca7da32
Bisrat Berhanu <bisratbnegus@gmail.com>
2026-02-11 14:56:08 +0300

    first commit

---
+++ DSA_Revivied/1.two-sum.py
@@ -0,0 +1,19 @@
+#
+# @lc app=leetcode id=1 lang=python3
+#
+# [1] Two Sum
+#
+
+# @lc code=start
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dic={}
+
+        for i in range(len(nums)):
+
+            if target-nums[i] in dic:
+
+                return [dic[target-nums[i]],i]
+
+            dic[nums[i]]=i
+# @lc code=end
