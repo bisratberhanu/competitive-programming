0135df2c16b7f18dbcf3f788a5f9114a5b93a957
Bisrat Berhanu <bisratbnegus@gmail.com>
2026-02-17 17:50:33 +0300

    Refactor 'reverseBits' method for improved efficiency(O(32))

--- DSA_Revivied/190.reverse-bits.py
+++ DSA_Revivied/190.reverse-bits.py
@@ -8,24 +8,14 @@
 class Solution:
     
     def reverseBits(self, n: int) -> int:
-        def int_to_bits(n):
-            if n <= 1:
-                return str(n)
-            bits = ""
-            while n != 0:
-                val, mod = divmod(n,2)
-                n = val
-                bits += str(mod)
-            return bits
-        reversed_bits = int_to_bits(n)
-        reversed_bits += "0" * (32 - len(reversed_bits))
-        reversed_num = 0
-        for i in range(31,-1,-1):
-            reversed_num +=  int(reversed_bits[i])*pow(2,31-i)
-        return reversed_num
+        reversed_n = 0
+        for i in range(32):
+            reversed_n = reversed_n << 1 | (n & 1)
+            n >>= 1
+        
+        return reversed_n
 
 
             
 
 # @lc code=end
-
